777fb4a6ec61130f031a37ce5a56f89049c82c39
Indibar <indibar@algo2trade.com>
2019-02-09 18:10:09 +0530

    MR UI Settings

--- Algo2TradeRobot/frmMomentumReversalSettings.Designer.vb
+++ Algo2TradeRobot/frmMomentumReversalSettings.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmMomentumReversalSettings
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,13 +20,16 @@ Partial Class frmMomentumReversalSettings
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMomentumReversalSettings))
         Me.btnSaveMomentumReversalSettings = New System.Windows.Forms.Button()
         Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtInstrumentDetalis = New System.Windows.Forms.TextBox()
+        Me.lblInstrumentDetails = New System.Windows.Forms.Label()
         Me.txtSignalTimeFrame = New System.Windows.Forms.TextBox()
         Me.txtTargetMultiplier = New System.Windows.Forms.TextBox()
         Me.txtMaxSLPercentage = New System.Windows.Forms.TextBox()
@@ -35,6 +38,7 @@ Partial Class frmMomentumReversalSettings
         Me.lblTargetMultiplier = New System.Windows.Forms.Label()
         Me.lblMaxSLPercentage = New System.Windows.Forms.Label()
         Me.lblCandleWickSizePercentage = New System.Windows.Forms.Label()
+        Me.opnFileMRSettings = New System.Windows.Forms.OpenFileDialog()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -43,7 +47,7 @@ Partial Class frmMomentumReversalSettings
         Me.btnSaveMomentumReversalSettings.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnSaveMomentumReversalSettings.ImageKey = "save-icon-36533.png"
         Me.btnSaveMomentumReversalSettings.ImageList = Me.ImageList1
-        Me.btnSaveMomentumReversalSettings.Location = New System.Drawing.Point(348, 11)
+        Me.btnSaveMomentumReversalSettings.Location = New System.Drawing.Point(456, 13)
         Me.btnSaveMomentumReversalSettings.Margin = New System.Windows.Forms.Padding(4)
         Me.btnSaveMomentumReversalSettings.Name = "btnSaveMomentumReversalSettings"
         Me.btnSaveMomentumReversalSettings.Size = New System.Drawing.Size(112, 58)
@@ -60,6 +64,9 @@ Partial Class frmMomentumReversalSettings
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.btnBrowse)
+        Me.GroupBox1.Controls.Add(Me.txtInstrumentDetalis)
+        Me.GroupBox1.Controls.Add(Me.lblInstrumentDetails)
         Me.GroupBox1.Controls.Add(Me.txtSignalTimeFrame)
         Me.GroupBox1.Controls.Add(Me.txtTargetMultiplier)
         Me.GroupBox1.Controls.Add(Me.txtMaxSLPercentage)
@@ -72,32 +79,61 @@ Partial Class frmMomentumReversalSettings
         Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
-        Me.GroupBox1.Size = New System.Drawing.Size(336, 183)
+        Me.GroupBox1.Size = New System.Drawing.Size(451, 226)
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
         '
+        'btnBrowse
+        '
+        Me.btnBrowse.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBrowse.Location = New System.Drawing.Point(404, 188)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(40, 23)
+        Me.btnBrowse.TabIndex = 10
+        Me.btnBrowse.Text = "..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtInstrumentDetalis
+        '
+        Me.txtInstrumentDetalis.Location = New System.Drawing.Point(175, 188)
+        Me.txtInstrumentDetalis.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtInstrumentDetalis.Name = "txtInstrumentDetalis"
+        Me.txtInstrumentDetalis.Size = New System.Drawing.Size(222, 22)
+        Me.txtInstrumentDetalis.TabIndex = 9
+        Me.txtInstrumentDetalis.Text = "G:\algo2trade\GitHub\Algo2Trade Live\Momentum Reversal Instrument Details.xlsx"
+        '
+        'lblInstrumentDetails
+        '
+        Me.lblInstrumentDetails.AutoSize = True
+        Me.lblInstrumentDetails.Location = New System.Drawing.Point(9, 191)
+        Me.lblInstrumentDetails.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblInstrumentDetails.Name = "lblInstrumentDetails"
+        Me.lblInstrumentDetails.Size = New System.Drawing.Size(121, 17)
+        Me.lblInstrumentDetails.TabIndex = 8
+        Me.lblInstrumentDetails.Text = "Instrument Details"
+        '
         'txtSignalTimeFrame
         '
-        Me.txtSignalTimeFrame.Location = New System.Drawing.Point(175, 138)
+        Me.txtSignalTimeFrame.Location = New System.Drawing.Point(175, 147)
         Me.txtSignalTimeFrame.Margin = New System.Windows.Forms.Padding(4)
         Me.txtSignalTimeFrame.Name = "txtSignalTimeFrame"
-        Me.txtSignalTimeFrame.Size = New System.Drawing.Size(135, 22)
+        Me.txtSignalTimeFrame.Size = New System.Drawing.Size(255, 22)
         Me.txtSignalTimeFrame.TabIndex = 7
         '
         'txtTargetMultiplier
         '
-        Me.txtTargetMultiplier.Location = New System.Drawing.Point(175, 100)
+        Me.txtTargetMultiplier.Location = New System.Drawing.Point(175, 107)
         Me.txtTargetMultiplier.Margin = New System.Windows.Forms.Padding(4)
         Me.txtTargetMultiplier.Name = "txtTargetMultiplier"
-        Me.txtTargetMultiplier.Size = New System.Drawing.Size(135, 22)
+        Me.txtTargetMultiplier.Size = New System.Drawing.Size(256, 22)
         Me.txtTargetMultiplier.TabIndex = 6
         '
         'txtMaxSLPercentage
         '
-        Me.txtMaxSLPercentage.Location = New System.Drawing.Point(175, 60)
+        Me.txtMaxSLPercentage.Location = New System.Drawing.Point(175, 64)
         Me.txtMaxSLPercentage.Margin = New System.Windows.Forms.Padding(4)
         Me.txtMaxSLPercentage.Name = "txtMaxSLPercentage"
-        Me.txtMaxSLPercentage.Size = New System.Drawing.Size(135, 22)
+        Me.txtMaxSLPercentage.Size = New System.Drawing.Size(256, 22)
         Me.txtMaxSLPercentage.TabIndex = 5
         '
         'txtCandleWickSizePercentage
@@ -105,13 +141,13 @@ Partial Class frmMomentumReversalSettings
         Me.txtCandleWickSizePercentage.Location = New System.Drawing.Point(175, 21)
         Me.txtCandleWickSizePercentage.Margin = New System.Windows.Forms.Padding(4)
         Me.txtCandleWickSizePercentage.Name = "txtCandleWickSizePercentage"
-        Me.txtCandleWickSizePercentage.Size = New System.Drawing.Size(135, 22)
+        Me.txtCandleWickSizePercentage.Size = New System.Drawing.Size(255, 22)
         Me.txtCandleWickSizePercentage.TabIndex = 4
         '
         'lblSignalTimeFrame
         '
         Me.lblSignalTimeFrame.AutoSize = True
-        Me.lblSignalTimeFrame.Location = New System.Drawing.Point(9, 142)
+        Me.lblSignalTimeFrame.Location = New System.Drawing.Point(9, 150)
         Me.lblSignalTimeFrame.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblSignalTimeFrame.Name = "lblSignalTimeFrame"
         Me.lblSignalTimeFrame.Size = New System.Drawing.Size(158, 17)
@@ -121,7 +157,7 @@ Partial Class frmMomentumReversalSettings
         'lblTargetMultiplier
         '
         Me.lblTargetMultiplier.AutoSize = True
-        Me.lblTargetMultiplier.Location = New System.Drawing.Point(9, 103)
+        Me.lblTargetMultiplier.Location = New System.Drawing.Point(9, 111)
         Me.lblTargetMultiplier.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblTargetMultiplier.Name = "lblTargetMultiplier"
         Me.lblTargetMultiplier.Size = New System.Drawing.Size(110, 17)
@@ -131,7 +167,7 @@ Partial Class frmMomentumReversalSettings
         'lblMaxSLPercentage
         '
         Me.lblMaxSLPercentage.AutoSize = True
-        Me.lblMaxSLPercentage.Location = New System.Drawing.Point(9, 64)
+        Me.lblMaxSLPercentage.Location = New System.Drawing.Point(9, 68)
         Me.lblMaxSLPercentage.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblMaxSLPercentage.Name = "lblMaxSLPercentage"
         Me.lblMaxSLPercentage.Size = New System.Drawing.Size(107, 17)
@@ -148,11 +184,14 @@ Partial Class frmMomentumReversalSettings
         Me.lblCandleWickSizePercentage.TabIndex = 0
         Me.lblCandleWickSizePercentage.Text = "Candle Wick Size %"
         '
+        'opnFileMRSettings
+        '
+        '
         'frmMomentumReversalSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(476, 198)
+        Me.ClientSize = New System.Drawing.Size(575, 241)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.btnSaveMomentumReversalSettings)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -161,7 +200,7 @@ Partial Class frmMomentumReversalSettings
         Me.MinimizeBox = False
         Me.Name = "frmMomentumReversalSettings"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Momentume Reversal  - Settings"
+        Me.Text = "Momentum Reversal  - Settings"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
@@ -179,4 +218,8 @@ Partial Class frmMomentumReversalSettings
     Friend WithEvents lblMaxSLPercentage As Label
     Friend WithEvents lblCandleWickSizePercentage As Label
     Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtInstrumentDetalis As TextBox
+    Friend WithEvents lblInstrumentDetails As Label
+    Friend WithEvents opnFileMRSettings As OpenFileDialog
 End Class
--- Algo2TradeRobot/frmMomentumReversalSettings.vb
+++ Algo2TradeRobot/frmMomentumReversalSettings.vb
@@ -1,17 +1,32 @@
-﻿Imports System.IO
+﻿Imports Utilities.DAL
+Imports System.IO
 Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.Threading
 
 Public Class frmMomentumReversalSettings
 
-    Private _MRSettings As MomentumReversalSettings = Nothing
+    Private _cts As CancellationTokenSource = Nothing
+    Private _MRSettings As MomentumReversalUserInputs = Nothing
     Private _MRSettingsFilename As String = Path.Combine(My.Application.Info.DirectoryPath, "MomentumReversalSettings.a2t")
+    'Private _InstrumentsData As Dictionary(Of String, MomentumReversalUserInputs.InstrumentDetails) = Nothing
+
+    Public Sub New(ByRef MRUserInputs As MomentumReversalUserInputs)
+        ' This call is required by the designer.
+        InitializeComponent()
+        ' Add any initialization after the InitializeComponent() call.
+        _MRSettings = MRUserInputs
+    End Sub
+
     Private Sub frmMomentumReversalSettings_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadSettings()
     End Sub
     Private Sub btnSaveMomentumReversalSettings_Click(sender As Object, e As EventArgs) Handles btnSaveMomentumReversalSettings.Click
         Try
+            _cts = New CancellationTokenSource
+            If _MRSettings Is Nothing Then _MRSettings = New MomentumReversalUserInputs
+            _MRSettings.InstrumentsData = Nothing
             ValidateInputs()
-            SaveSettings()
+            'SaveSettings()
             Me.Close()
         Catch ex As Exception
             MsgBox(String.Format("The following error occurred: {0}", ex.Message), MsgBoxStyle.Critical)
@@ -21,34 +36,27 @@ Public Class frmMomentumReversalSettings
         If File.Exists(_MRSettingsFilename) Then
             Dim fs As Stream = New FileStream(_MRSettingsFilename, FileMode.Open)
             Dim bf As BinaryFormatter = New BinaryFormatter()
-            _MRSettings = CType(bf.Deserialize(fs), MomentumReversalSettings)
+            _MRSettings = CType(bf.Deserialize(fs), MomentumReversalUserInputs)
             fs.Close()
             txtCandleWickSizePercentage.Text = _MRSettings.CandleWickSizePercentage
             txtMaxSLPercentage.Text = _MRSettings.MaxStoplossPercentage
             txtTargetMultiplier.Text = _MRSettings.TargetMultiplier
             txtSignalTimeFrame.Text = _MRSettings.SignalTimeFrame
+            txtInstrumentDetalis.Text = _MRSettings.InstrumentDetailsFilePath
         End If
     End Sub
     Private Sub SaveSettings()
-        If _MRSettings Is Nothing Then _MRSettings = New MomentumReversalSettings
         _MRSettings.CandleWickSizePercentage = txtCandleWickSizePercentage.Text
         _MRSettings.MaxStoplossPercentage = txtMaxSLPercentage.Text
         _MRSettings.TargetMultiplier = txtTargetMultiplier.Text
         _MRSettings.SignalTimeFrame = txtSignalTimeFrame.Text
+        _MRSettings.InstrumentDetailsFilePath = txtInstrumentDetalis.Text
 
         Dim fs As Stream = New FileStream(_MRSettingsFilename, FileMode.OpenOrCreate)
         Dim bf As BinaryFormatter = New BinaryFormatter()
         bf.Serialize(fs, _MRSettings)
         fs.Close()
     End Sub
-
-    <Serializable>
-    Public Class MomentumReversalSettings
-        Public CandleWickSizePercentage As Decimal
-        Public MaxStoplossPercentage As Decimal
-        Public TargetMultiplier As Decimal
-        Public SignalTimeFrame As Integer
-    End Class
     Private Function ValidateNumbers(ByVal startNumber As Integer, ByVal endNumber As Integer, ByVal inputTB As TextBox) As Boolean
         Dim ret As Boolean = False
         If IsNumeric(inputTB.Text) Then
@@ -59,10 +67,27 @@ Public Class frmMomentumReversalSettings
         If Not ret Then Throw New ApplicationException(String.Format("{0} cannot have a value < {1} or > {2}", inputTB.Tag, startNumber, endNumber))
         Return ret
     End Function
+    Private Sub ValidateFile()
+        _MRSettings.FillInstrumentDetails(txtInstrumentDetalis.Text, _cts)
+    End Sub
     Private Sub ValidateInputs()
-        ValidateNumbers(0, 999, txtCandleWickSizePercentage)
-        ValidateNumbers(0, 999, txtMaxSLPercentage)
-        ValidateNumbers(0, 999, txtTargetMultiplier)
-        ValidateNumbers(1, 60, txtSignalTimeFrame)
+        'ValidateNumbers(0, 999, txtCandleWickSizePercentage)
+        'ValidateNumbers(0, 999, txtMaxSLPercentage)
+        'ValidateNumbers(0, 999, txtTargetMultiplier)
+        'ValidateNumbers(1, 60, txtSignalTimeFrame)
+        ValidateFile()
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        opnFileMRSettings.ShowDialog()
+    End Sub
+
+    Private Sub opnFileMRSettings_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles opnFileMRSettings.FileOk
+        Dim extension As String = Path.GetExtension(opnFileMRSettings.FileName)
+        If extension = ".xlsx" OrElse extension = ".xls" Then
+            txtInstrumentDetalis.Text = opnFileMRSettings.FileName
+        Else
+            MsgBox("File Type not supported. Please Try again.", MsgBoxStyle.Critical)
+        End If
     End Sub
 End Class
